e66e0b26c10ec5f5f784ed03d8af029239347ea5
Noka871 <58460996+Noka871@users.noreply.github.com>
2025-12-25 06:23:16 +0700

    Delete save_report.bat

--- save_report.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "C:\Users\Admin\Desktop\LispLab"
-
-"C:\Program Files (x86)\clisp-2.49\clisp.exe" -q -x "(with-open-file (f \"report.txt\" :direction :output :if-exists :supersede) (load \"lab7.lisp\") (format f \"Lab Work #1, Variant 7~%\") (format f \"Function: remove-deep~%~%\") (format f \"Test results:~%~%\") (format f \"1. ~a~%\" (remove-deep 'a '(a b c))) (format f \"2. ~a~%\" (remove-deep 'a '(a (a b) c))) (format f \"3. ~a~%\" (remove-deep 'a '(1 2 3))) (format f \"4. ~a~%~%\" (remove-deep 'a '(a (b (a c) a) d))) (format f \"All tests passed.~%\"))"
-
-type report.txt
-echo.
-echo Report saved to 'report.txt'
-pause
