e6e38095a3015fc28d66ec6262f98dce05088f6f
Noka871 <58460996+Noka871@users.noreply.github.com>
2025-12-25 07:19:49 +0700

    Add files via upload

--- lab7.lisp
+++ lab7.lisp
@@ -1,19 +1,52 @@
 ;;; Laboratory work #1, Variant 7
-;;; Function: remove-deep
+;;; Real tasks: summa_digits and f
 
-(defun remove-deep (x s)
+;;; FUNCTION 1: summa_digits
+(defun summa_digits (n)
+  (cond
+    ((< n 10) n)
+    (t (+ (mod n 10) 
+          (summa_digits (truncate n 10))))))
+
+;;; FUNCTION 2: f (main version)
+(defun f (s)
+  (append
+    (mapcar #'- (remove-if-not #'minusp s))
+    (mapcar #'- (remove-if #'minusp s))))
+
+;;; FUNCTION 2: f (recursive version)
+(defun f-recursive (s)
   (cond
     ((null s) nil)
-    ((listp (first s))
-     (cons (remove-deep x (first s))
-           (remove-deep x (rest s))))
-    ((equal (first s) x)
-     (remove-deep x (rest s)))
-    (t
-     (cons (first s) (remove-deep x (rest s))))))
+    ((< (first s) 0)
+     (cons (- (first s)) 
+           (f-recursive (rest s))))
+    (t 
+     (let ((rest-result (f-recursive (rest s))))
+       (append rest-result (list (- (first s))))))))
 
-;;; Test cases (uncomment to run manually):
-;; (remove-deep 'a '(a b c))
-;; (remove-deep 'a '(a (a b) c))
-;; (remove-deep 'a '(1 2 3))
-;; (remove-deep 'a '(a (b (a c) a) d))
+;;; TEST FUNCTION
+(defun test-all ()
+  (format t "~%~%=== TESTING VARIANT 7 ===~%~%")
+  
+  (format t "Function 1: summa_digits~%")
+  (format t "  (summa_digits 0) -> ~a~%" (summa_digits 0))
+  (format t "  (summa_digits 5) -> ~a~%" (summa_digits 5))
+  (format t "  (summa_digits 123) -> ~a~%" (summa_digits 123))
+  (format t "  (summa_digits 1001) -> ~a~%~%" (summa_digits 1001))
+  
+  (format t "Function 2: f~%")
+  (format t "  (f '(4 -8 6 -9 -7)) -> ~a~%" (f '(4 -8 6 -9 -7)))
+  (format t "  (f '()) -> ~a~%" (f '()))
+  (format t "  (f '(1 2 3)) -> ~a~%" (f '(1 2 3)))
+  (format t "  (f '(-1 -2 -3)) -> ~a~%" (f '(-1 -2 -3)))
+  (format t "  (f '(0 -5 10 -15)) -> ~a~%~%" (f '(0 -5 10 -15)))
+  
+  (format t "Function 2 (recursive): f-recursive~%")
+  (format t "  (f-recursive '(4 -8 6 -9 -7)) -> ~a~%" (f-recursive '(4 -8 6 -9 -7)))
+  
+  (format t "~%=== ALL TESTS COMPLETED ===~%")
+  t)
+
+;;; Uncomment to auto-run tests:
+;; (test-all)
